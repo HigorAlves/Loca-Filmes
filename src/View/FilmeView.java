26e65af9038410b231d2ee8439b72ee60a070e3c
Higor Alves <Goodyearalves@gmail.com>
2017-12-09 17:58:57 -0200

    Filme View Init

--- src/View/FilmeView.java
+++ src/View/FilmeView.java
@@ -1,12 +1,23 @@
 package View;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 
 public class FilmeView extends JInternalFrame implements View {
 	private static FilmeView INSTANCIA = null;
 	
+	private JPanel pane;
+	
 	private FilmeView() {
+		super("Filme",true,true,true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setSize(1300,300);
 		
+		setContentPane(createGui());
+	}
+	
+	private JPanel createGui() {
+		return pane;
 	}
 	
 	public static FilmeView getInstancia() {

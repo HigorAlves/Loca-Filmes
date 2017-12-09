872f4a032e82b55441264208209886d0e0557a91
Higor Alves <Goodyearalves@gmail.com>
2017-12-09 20:21:34 -0200

    Database init

--- src/Model/DataBase.java
+++ src/Model/DataBase.java
@@ -1,5 +1,25 @@
 package Model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DataBase {
+	private static DataBase instancia = null;
 	
+	private List funcionarios;
+	private List clientes;
+	private List filmes;
+	
+	private DataBase() {
+		funcionarios = new LinkedList<Funcionario>();
+		clientes = new LinkedList<Cliente>();
+		filmes = new LinkedList<Filme>();
+	}
+	
+	public static DataBase getInstance() {
+		if (instancia == null) {
+			instancia = new DataBase();
+		}
+		return instancia;
+	}
 }
